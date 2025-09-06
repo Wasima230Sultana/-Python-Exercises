58125c2e4822a487dcae4b45c9ba257855c4e6b3
wasimasultana230 <wasimasultana230@gmail.com>
2025-09-06 16:43:24 +0600

    Arithmetic operation

---
+++ Arithmetic-Operation.py
@@ -0,0 +1,34 @@
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+# Addition
+Total = num1 + num2
+print(("The sum of {} and {} is {}.".format(num1, num2, Total)))
+
+# Subtraction
+difference = num1 - num2
+print(("The difference of {} and {} is {}.".format(num1, num2, difference)))
+
+# Multiplication
+answer = num1 * num2
+print(("The multiplication of {} and {} is {}.".format(num1, num2, answer)))
+
+# Division
+div = num1 / num2
+print(("The division of {} and {} is {}.".format(num1, num2, div)))
+
+# Floor Division
+floorDiv = num1 // num2
+print(("The floor division of {} and {} is {}.".format(num1, num2, floorDiv)))
+
+# Modulus
+remainder = num1 % num2
+print(("The remainder of {} and {} is {}.".format(num1, num2, remainder)))
+
+# Exponentiation
+power = num1 ** num2
+print(("The answer of {} power {} is {}.".format(num1, num2, power)))
+
+
+
