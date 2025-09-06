93db37abd74efc05e3bff0dee1ace7ab2f6b714f
wasimasultana230 <wasimasultana230@gmail.com>
2025-09-06 16:21:48 +0600

    About Myself

--- myinfo.py
+++ myinfo.py
@@ -1,1 +1,5 @@
-print("I am Wasima Sultana, a student of Computer Science and\n Engineering in Metropolitan University. I am from Sylhet.My hobby\nis problem solving, especially working on real-world problem and I enjoy\n exploring new topics to gain knowledge.")
+info = '''I am Wasima Sultana, a student of Computer Science and
+ Engineering in Metropolitan University. I am from Sylhet.
+ My hobby is problem solving, especially working on real-world problem 
+ and I enjoy exploring new topics to gain knowledge.'''
+print(info)
