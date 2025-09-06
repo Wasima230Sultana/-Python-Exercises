c1ab6ac20f42d481b99bf479066d9410c9ab5a37
wasimasultana230 <wasimasultana230@gmail.com>
2025-09-06 21:43:22 +0600

    Simple interest

---
+++ calculate-simple.py
@@ -0,0 +1,6 @@
+p = float(input("Enter the number :"))
+T = float(input("Enter the number :"))
+R = float(input("Enter the number :"))
+
+simpleInterest = (p * T * R ) / 100
+print(f"Simple Interest {simpleInterest:.2f}")
