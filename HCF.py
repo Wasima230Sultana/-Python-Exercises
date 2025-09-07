ddbecdc8ca5ceddce2286f7faf1c207b09289a88
wasimasultana230 <wasimasultana230@gmail.com>
2025-09-07 11:42:24 +0600

    Highest Common Factor

---
+++ HCF.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+def hcf_is(x, y):
+    if(x < y):
+        smaller = x
+    else:
+        smaller = y
+    for i in range(1, smaller+1):
+        if(x % i == 0) and (y % i == 0):
+            hcf = i
+    return hcf
+
+
+print(f"H.C.F is {(hcf_is(num1, num2))}")
