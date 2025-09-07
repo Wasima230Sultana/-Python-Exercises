d0169a20296202b573bafd270019b1c0a8c61dce
wasimasultana230 <wasimasultana230@gmail.com>
2025-09-07 12:05:16 +0600

    Add check in leap year

--- LeapYear.py
+++ LeapYear.py
@@ -1,12 +1,29 @@
-year = int(input("Enter the year : "))
+ask_again = True
 
-if(year%400 == 0):
-    print(f"{year} is a Leap Year.")
-elif(year%100 == 0):
-    print(f"{year} is not a Leap Year.")
-elif(year%4 == 0):
-    print(f"{year} is a Leap Year.")
-else:
-    print(f"{year} is not a Leap Year.")
+while ask_again:
+    year = int(input("Enter the year : "))
+
+    if(year%400 == 0):
+       print(f"{year} is a Leap Year.")
+    elif(year%100 == 0):
+       print(f"{year} is not a Leap Year.")
+    elif(year%4 == 0):
+       print(f"{year} is a Leap Year.")
+    else:
+       print(f"{year} is not a Leap Year.")
+    question = input("Do you want check another year ? Y/N  ").lower()
+    if(question == 'y'):
+       ask_again = True
+    elif(question == 'n'):
+       print("Stop")
+       ask_again = False
+    else:
+       print("Invalid")
+       ask_again = False
+       
+
+
+
+
 
 
